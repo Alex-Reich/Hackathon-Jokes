96ad380782e9b524701f7c0636ce54d4dcc80764
Weston Dockstader <dockstaderweston@gmail.com>
2018-05-18 13:54:49 -0600

    helloworld.vue connenctions and store updates

--- www/src/components/HelloWorld.vue
+++ www/src/components/HelloWorld.vue
@@ -6,21 +6,23 @@
       <button @click="sForm=2">Sign Up</button>
       <div v-if="sForm==1">
         <form v-on:submit.prevent="">
-          <input type="text" name="userName" placeholder="Enter User Name">
+          <input type="text" name="userName" placeholder="Enter User Name" v-model="user.userName">
           <button @click="showLogin=1" type="submit">Submit</button>
         </form>
       </div>
       <div v-if="sForm==2">
           <form v-on:submit.prevent="">
-            <input type="text" name="userName" placeholder="Select User Name">
+            <input type="text" name="userName" placeholder="Select User Name" v-model="user.userName">
             <button @click="showLogin=1" type="submit">Submit</button>
           </form>
         </div>
     </div>
     <!-- End of sign in sign up code -->
+
+
     <div class="post" v-if="showLogin==1">
         <button @click="showLogin=0">Logout</button>
-
+        <h1 v-if="activeUser._id">Hello, {{activeUser.name}}</h1>
     </div>
   </div>
 </template>
@@ -31,8 +33,26 @@ export default {
   data () {
     return {
       showLogin: 0,
-      sForm: 0
+      sForm: 0,
+      activeUser:{},
+      user:{
+        userName: ''
+      },
     }
+  },
+  mounted(){
+    this.$store.dispatch('getPosts')
+  },
+  computed:{
+    posts(){
+      return this.$store.state.posts
+    }
+  },
+  methods:{
+    addUser(){
+      this.$store.dispatch('addUser',this.user)
+    },
+    getUser
   }
 }
 </script>
